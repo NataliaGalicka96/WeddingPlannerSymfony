7e075a605245b02e18766d08c2a5faaa613b7edb
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-11 06:44:51 +0200

    Add new entites: CheckListCategory and CheckListPodcategory

---
+++ src/Repository/CheckListCategoryRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CheckListCategory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<CheckListCategory>
+ *
+ * @method CheckListCategory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CheckListCategory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CheckListCategory[]    findAll()
+ * @method CheckListCategory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CheckListCategoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CheckListCategory::class);
+    }
+
+    public function add(CheckListCategory $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(CheckListCategory $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return CheckListCategory[] Returns an array of CheckListCategory objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?CheckListCategory
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
