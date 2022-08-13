b0b53c2b345d4a64ab282312da8de8c88fac8669
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-13 14:26:55 +0200

    Create form to add new guest

--- src/Controller/GuestController.php
+++ src/Controller/GuestController.php
@@ -5,18 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\GuestType;
+use App\Entity\Guest;
 
 class GuestController extends AbstractController
 {
     #[Route('/guest', name: 'app_guest')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         if(!$this->getUser()){
             return $this->redirectToRoute('app_index');
         }
+
+        $form = $this->createForm(GuestType::class);
+
+
+
         
         return $this->render('guest/index.html.twig', [
             'controller_name' => 'GuestController',
+            'guestform' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/GuestType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Guest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class GuestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                'label' => 'Imię i nazwisko',
+                'attr' => [
+                    'placeholder' => "Imię i nazwisko"
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating mb-3'
+                ]
+            ])
+            ->add('is_confirmed', CheckboxType::class,[
+                'label' => 'Potwierdzona obecność'
+            ])
+            ->add('is_accommodation', CheckboxType::class,[
+                'label' => 'Nocleg'
+            ])
+            ->add('transport', CheckboxType::class,[
+                'label' => 'Transport'
+            ])
+            ->add('is_adult', CheckboxType::class,[
+                'label' => 'Osoba dorosła'
+            ])
+            ->add('is_child_under_3_years', CheckboxType::class,[
+                'label' => 'Dziecko poniżej 3 roku życia'
+            ])
+            ->add('is_child_between_3_12_years', CheckboxType::class,[
+                'label' => 'Dziecko 3-12 lat'
+            ])
+            ->add('special_diet', CheckboxType::class,[
+                'label' => 'Specjalna dieta'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Guest::class,
+        ]);
+    }
+}
