7c6a3dbebca56bf71c4e9018247b7eb5c1b203b6
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-13 13:41:41 +0200

    Add new contact to database and display

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Form\ContactType;
 use App\Entity\Contact;
@@ -12,7 +13,7 @@ use App\Entity\Contact;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         if(!$this->getUser()){
             return $this->redirectToRoute('app_index');
@@ -30,11 +31,41 @@ class ContactController extends AbstractController
         $contacts = $em->getRepository(Contact::class)->getContact($userId);
 
 
-        $form = $this->createForm(ContactType::class);
         
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            if($this->getUser())
+            {
+                $newContact = new Contact();
+
+                $newContact->setUser($this->getUser());
+                $newContact->setName($form->get('name')->getData());
+                $newContact->setEmail($form->get('email')->getData());
+                $newContact->setPhoneNumber($form->get('phoneNumber')->getData());
+                $newContact->setStreet($form->get('street')->getData());
+                $newContact->setHomeNumber($form->get('homeNumber')->getData());
+                $newContact->setCity($form->get('city')->getData());
+                $newContact->setPostalcode($form->get('postalcode')->getData());
+                try{
+                    $em->persist($newContact);
+                    $em->flush();
+                    $this->addFlash('success', "Dodano nowy kontakt!");
+                }catch(\Exception $e) {
+                    $this->addFlash('error', 'Wystąpił nieoczekiwany błąd!');
+                }
+                
+
+            }
+        }
+
         return $this->render('contact/index.html.twig', [
             'contacts' => $contacts,
             'formContact' => $form->createView(),
         ]);
     }
+
+
 }
