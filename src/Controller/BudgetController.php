acf3faf472f23ef1f1b44318b057f772c041ea07
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-17 13:11:01 +0200

    Add Entity with default category of exenpses

--- src/Controller/BudgetController.php
+++ src/Controller/BudgetController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\BudgetCategory;
+
 class BudgetController extends AbstractController
 {
     #[Route('/budget', name: 'app_budget')]
@@ -16,8 +18,21 @@ class BudgetController extends AbstractController
             return $this->redirectToRoute('app_index');
         }
 
+        /** 
+         * @var User $user 
+         * */
+        $user = $this->getUser();
+        if(!empty($user)){
+        $userId = $user->getId();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        
+        $categoryBudget = $em->getRepository(BudgetCategory::class)->getCategoryOfBudget();
+    
+
         return $this->render('budget/index.html.twig', [
-            'controller_name' => 'BudgetController',
+            'budgetCategory' => $categoryBudget,
         ]);
     }
 }
