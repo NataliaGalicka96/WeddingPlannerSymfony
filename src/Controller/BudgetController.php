a315bcb3db9c8daf1a281bc5c2b853e6a4ce7abf
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-18 13:31:21 +0200

    Add new method to display details of expense

--- src/Controller/BudgetController.php
+++ src/Controller/BudgetController.php
@@ -8,13 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\BudgetCategory;
 use App\Entity\Expenses;
+use App\Entity\Budget;
+use Symfony\Component\HttpFoundation\Request;
 
 class BudgetController extends AbstractController
 {
     #[Route('/budget', name: 'app_budget')]
     public function index(): Response
     {
-
         if(!$this->getUser()){
             return $this->redirectToRoute('app_index');
         }
@@ -31,10 +32,66 @@ class BudgetController extends AbstractController
         
         $categoryBudget = $em->getRepository(BudgetCategory::class)->getCategoryOfBudget();
         $expenses = $em->getRepository(Expenses::class)->getExpensesAssignedToUser($userId);
+        $budget = $em->getRepository(Budget::class)->getBudgetAssignedToUser($userId);
+        $alreadyPaidGroupByCategory = $em->getRepository(Expenses::class)->getSumOfAlreadyPaid($userId);
+        $sumOfalreadyPaid = $em->getRepository(Expenses::class)->sumOfAllExpenses($userId);
+        $detailsOfExpense = $em->getRepository(Expenses::class)->getDetailsOfExpense($userId);
+
+
+
+
+
+
 
         return $this->render('budget/index.html.twig', [
             'budgetCategory' => $categoryBudget,
-            'expenses' => $expenses
+            'expenses' => $expenses,
+            'budget' => $budget,
+            'alreadyPaid' => $alreadyPaidGroupByCategory,
+            'sumOfAlreadyPaid' => $sumOfalreadyPaid,
+            'details' => $detailsOfExpense
         ]);
     }
+
+    #[Route('/budget/set', name: 'set_budget', methods: 'POST')]
+    public function setBudget(Request $request): Response
+    {
+
+        $budgetFromForm = trim($request->request->get('budget'));
+        $budget = (float)number_format((float)$budgetFromForm, 2, '.', '');
+
+        var_dump($budget);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $setBudget = new Budget();
+
+        $setBudget->setBudget($budget);
+        $setBudget->setUser($this->getUser());
+
+        
+
+        $entityManager->persist($setBudget);
+        $entityManager->flush();
+
+
+        return $this->redirectToRoute('app_budget');
+    }
 }
+/*
+    if(empty($title))
+        return $this->redirectToRoute('app_check_list');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new CheckList();
+
+        $task->setCategoryName($category);
+        $task->setTask($title);
+        $task->setUser($this->getUser());
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_check_list');
+        */
---
+++ src/Entity/Budget.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BudgetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+
+#[ORM\Entity(repositoryClass: BudgetRepository::class)]
+class Budget
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'budget')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $budget = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getBudget(): ?string
+    {
+        return $this->budget;
+    }
+
+    public function setBudget(string $budget): self
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+}
