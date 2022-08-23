da050e2334e87a7e0f1049aa6a83fac95e928695
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-08-23 15:10:29 +0200

    Add some change in css, try edit Contant

--- public/assets/js/timeToWedding.js
+++ public/assets/js/timeToWedding.js
@@ -1,19 +1,12 @@
-let weddingDate = document.getElementById('time').innerHTML;
-console.log(weddingDate);
-
-let year = weddingDate.substring(0, 4);
-let month = weddingDate.substring(5, 7);
-let day = weddingDate.substring(8, 11);
-let hour = weddingDate.substring(11, 13);
-let minutes = weddingDate.substring(14, 16);
-//let second = weddingDate.substring(17, 19);
-//console.log(second);
 
 
+/*
 
 function czasDoWydarzenia(year, month, day, hour, minutes) {
     var aktualnyCzas = new Date();
     var dataWydarzenia = new Date(year, month, day, hour, minutes);
+    
+    var pozostaleDni = 
     var pozostalyCzas = dataWydarzenia.getTime() - aktualnyCzas.getTime();
 
     if (pozostalyCzas > 0) {
@@ -45,3 +38,61 @@ window.onload = function () {
     setInterval("document.getElementById(idElement).innerHTML = czasDoWydarzenia(year, month, day, hour, minutes)", 1000);
 
 };
+
+*/
+
+function makeTimer() {
+
+    let weddingDate = document.getElementById('time').innerHTML;
+    console.log(weddingDate);
+
+    let year = weddingDate.substring(0, 4);
+    let month = weddingDate.substring(5, 7);
+    let day = weddingDate.substring(8, 11);
+    let hour = weddingDate.substring(11, 13);
+    let minutesOfWeddingDate = weddingDate.substring(14, 16);
+    let secondsOfWeddingDate = weddingDate.substring(17, 19);
+
+    let daysToWedding = document.getElementById("days");
+    let hourToWedding = document.getElementById("hours");
+    let minutesToWedding = document.getElementById("minutes");
+    let secondToWedding = document.getElementById("seconds");
+    //console.log(daysToWedding);
+
+    var endTime = new Date(year, month, day, hour, minutesOfWeddingDate, secondsOfWeddingDate);
+    endTime = (Date.parse(endTime) / 1000);
+    console.log(endTime);
+
+    var now = new Date();
+    now = (Date.parse(now) / 1000);
+
+    var timeLeft = endTime - now;
+
+    var days = Math.floor(timeLeft / 86400);
+    console.log(days);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    daysToWedding.innerHTML = days + "<span>Dni</span>";
+    hourToWedding.innerHTML = hours + "<span>Godzin</span>";
+    minutesToWedding.innerHTML = minutes + "<span>Minut</span>";
+    secondToWedding.innerHTML = seconds + "<span>Sekund</span>";
+}
+
+setInterval(function () { makeTimer(); }, 1000)
+
+/*
+window.onload = function () {
+
+    idElement = "timer";
+    document.getElementById(idElement).innerHTML = makeTimer();
+    setInterval("document.getElementById(idElement).innerHTML = makeTimer()", 1000);
+
+};
+
+*/
